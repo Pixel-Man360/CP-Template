463e4665b43c27a6e57a6b0a63b17e01b8f153a8
Pixel-Man360 <muntasir.chowdury360@gmail.com>
2022-12-10 02:26:12 +0600

    Base Template

---
+++ CP-Template.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define test ll b; cin >> b; while(b--)
+#define FST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+#define f(x, n) for(ll i = x; i < n; i++)
+#define fj(x, n) for(ll j = x; j < n; j++)
+#define frev(x, n) for(ll i = n; i >= x ; i--)
+#define frevj(x, n) for(ll j = n; j >= x ; j--)
+
+#define nl cout << "\n";
+#define yes cout << "YES";
+#define no cout << "NO";
+
+using namespace std;
+
+ll __gcd (ll a, ll b) 
+{
+    return b ? __gcd (b, a % b) : a;
+}
+
+void Solve()
+{
+    
+}
+
+
+int main()
+{
+    FST;
+    
+    //ll c = 1;
+
+    test 
+    {
+        //cout << "Case " << c << ": ";
+        Solve();
+       // c++;
+        nl
+    } 
+    
+}
